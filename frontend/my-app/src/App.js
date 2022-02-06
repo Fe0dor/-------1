815dab8ffdb5b251896dd0434b13d91b180e4450
Fedor <b0risovfeodor@yandex.ru>
2022-02-06 21:44:57 +0300

    add edit button

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -6,65 +6,92 @@ const json = {
     { name: "Мирофбар", category: "Развлечения", price: 1800 },
     { name: "ВкусВилл", category: "Еда", price: 560 },
     { name: "Каток", category: "Развлечения", price: 450 },
+    { name: "ЦППК", category: "Обязательные", price: 27 },
+    { name: "Общага", category: "Обязательные", price: 751 },
   ]
 }
+
 
 function App() {
   return (
     <div className="App container">
-      <ExpenseList expenses={json.expenses} />
+      <Expenses expenses={json.expenses} />
     </div>
   );
 }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      Number(this.props.time)
-    );
-  }
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { date: new Date() };
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       Number(this.props.time)
+//     );
+//   }
 
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Привет, мир!</h1>
-        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
 
-// class Note extends React.Component {
 //   render() {
 //     return (
-//       <div class="card">
-//         <div class="card-body">
-//           <h5 class="card-title">{this.props.name}</h5>
-//           <h6 class="card-subtitle mb-2 text-muted">{this.props.category}</h6>
-//           <a href="#" class="card-link">Card link</a>
-//           <a href="#" class="card-link">Another link</a>
-//         </div>
+//       <div>
+//         <h1>Привет, мир!</h1>
+//         <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
 //       </div>
 //     );
 //   }
 // }
 
+class Expenses extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isEdit: false, expenses: props.expenses};
+    this.edit = this.edit.bind(this);
+  }
+
+  edit() {
+    this.setState(
+      function (state) {
+        return {isEdit: !state.isEdit};
+      }
+    );
+    console.log("1");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='display-1 pt-2'>Расходы</h1>
+        <ExpenceEditButton edit={this.edit} isEdit={this.state.isEdit} />
+        <ExpenseList expenses={json.expenses} isEdit={this.state.isEdit}/>
+      </div>
+    );
+  }
+}
+
+class ExpenceEditButton extends React.Component {
+  render() {
+    return (
+      <div className="d-grid gap-2 pb-2">
+        <button className="btn btn-primary" type="button" onClick={this.props.edit}>
+          {this.props.isEdit ? 'Отменить' : 'Изменить'}
+        </button>
+      </div>
+    )
+  }
+}
 
 class ExpenseList extends React.Component {
   constructor(props) {
@@ -80,7 +107,6 @@ class ExpenseList extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='display-1 pt-2'>Расходы</h1>
         <ul className='list-group'>
           {this.state.expensesList}
         </ul>
