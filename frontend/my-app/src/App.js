71bc63232a39b8032272a57840784ae7bfa92e10
Fedor <b0risovfeodor@yandex.ru>
2022-02-12 22:21:55 +0300

    last

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 const json = {
-  expenses: [
-    { name: "Мирофбар", category: "Развлечения", price: 1800 },
-    { name: "ВкусВилл", category: "Еда", price: 560 },
-    { name: "Каток", category: "Развлечения", price: 450 },
-    { name: "ЦППК", category: "Обязательные", price: 27 },
-    { name: "Общага", category: "Обязательные", price: 751 },
-  ]
+  expenses: {
+    "+1": {name: "Мирофбар", category: "Развлечения", price: 1800 },
+    "+2": {name: "ВкусВилл", category: "Еда", price: 560 },
+    "+3": {name: "Каток", category: "Развлечения", price: 450 },
+    "+4": {name: "ЦППК", category: "Обязательные", price: 27 },
+    "+5": {name: "Общага", category: "Обязательные", price: 751 },
+  }
 }
 
 
@@ -16,55 +16,41 @@ function App() {
   return (
     <div className="App container">
       <Expenses expenses={json.expenses} />
+      
     </div>
   );
 }
 
 
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
+class Modal extends React.Component {
+  render() {
+    return (
+      <div className='modalwindow'>
+        <div className='modal__backdrop' onClick={this.props.closeFunction}></div>
+        <div className='modal__content container'>
+          <div className='modal__header'>
+            <button className='btn-close text-reset' onClick={this.props.closeFunction}></button>
+          </div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 
-//   componentDidMount() {
-//     this.timerID = setInterval(
-//       () => this.tick(),
-//       Number(this.props.time)
-//     );
-//   }
+}
 
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//   }
-
-//   tick() {
-//     this.setState({
-//       date: new Date()
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Привет, мир!</h1>
-//         <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
 
 class Expenses extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isEdit: false, expenses: props.expenses};
+    this.state = { isEdit: false, expenses: props.expenses };
     this.edit = this.edit.bind(this);
   }
 
   edit() {
     this.setState(
       function (state) {
-        return {isEdit: !state.isEdit};
+        return { isEdit: !state.isEdit };
       }
     );
     console.log("1");
@@ -74,14 +60,15 @@ class Expenses extends React.Component {
     return (
       <div>
         <h1 className='display-1 pt-2'>Расходы</h1>
-        <ExpenceEditButton edit={this.edit} isEdit={this.state.isEdit} />
-        <ExpenseList expenses={json.expenses} isEdit={this.state.isEdit}/>
+        <ExpenseEditButton edit={this.edit} isEdit={this.state.isEdit} />
+        <ExpenseList expenses={json.expenses} isEdit={this.state.isEdit} />
+        {this.state.isEdit && <Modal closeFunction={this.edit}><h1>hello</h1></Modal>}
       </div>
     );
   }
 }
 
-class ExpenceEditButton extends React.Component {
+class ExpenseEditButton extends React.Component {
   render() {
     return (
       <div className="d-grid gap-2 pb-2">
@@ -96,19 +83,38 @@ class ExpenceEditButton extends React.Component {
 class ExpenseList extends React.Component {
   constructor(props) {
     super(props);
-    const expenses = props.expenses;
-    this.state = {
-      expensesList: expenses.map(
-        (ex, i) => <ExpenseItem key={i.toString()} name={ex.name} category={ex.category} price={ex.price} />
-      )
-    };
+    this.openItemMore = this.openItemMore.bind(this);
+    this.editItem = this.editItem.bind(this);
+    this.state = { expenses: props.expenses, isOpenMore: false };
+  }
+
+  openItemMore(key) {
+    this.setState({isOpenMore: key});
+  }
+
+  editItem(expense, key) {
+    if (expense.key === undefined) {
+      console.log(2);
+    } else {
+      this.setState((state) => {
+        let expenses = state.expenses;
+        expenses[key] = expense;
+        return {expenses: expenses, isOpenMore: false};
+      });
+    }
   }
 
   render() {
+    const expenses = this.state.expenses;
+    let expenseList =[];
+    for (let key in expenses) {
+      expenseList.push(<ExpenseItem key={key} mykey={key} expense={expenses[key]} editItem={this.openItemMore} />)
+    }
     return (
       <div>
+        {this.state.isOpenMore && <ExpenseItemMore expense={this.state.expenses[this.state.isOpenMore]} returnExpense={this.editItem}/>}
         <ul className='list-group'>
-          {this.state.expensesList}
+          {expenseList}
         </ul>
       </div>
     );
@@ -119,15 +125,28 @@ class ExpenseList extends React.Component {
 class ExpenseItem extends React.Component {
   render() {
     return (
-      <li className='list-group-item d-flex justify-content-between align-items-start'>
+      <li className='list-group-item d-flex justify-content-between align-items-start' onClick={this.props.editItem(this.props.mykey)}>
         <div className='ms-2 me-auto'>
-          <div className='fw-bold'>{this.props.name}</div>
-          {this.props.category}
+          <div className='fw-bold'>{this.props.expense.name}</div>
+          {this.props.expense.category}
         </div>
-        <span className='badge bg-secondary rounded-pill'>{this.props.price}</span>
+        <span className='badge bg-secondary rounded-pill'>{this.props.expense.price}</span>
       </li>
     );
   }
 }
+
+
+class ExpenseItemMore extends React.Component {
+  render() {
+    return (
+      <Modal>
+        
+      </Modal>
+    );
+  }
+}
+
+
 
 export default App;
